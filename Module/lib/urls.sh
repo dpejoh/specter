76ef10d25941e4328a9c12f256f7f05753e5c03e
khx <106915370+dpejoh@users.noreply.github.com>
2026-05-03 01:19:00 +0100

    refactor: rename CDN from yuribin to rawbin
    feat: remove dead RKA feature (PassIt not publicly available)
    
    - Delete features/rka.sh, rka/jsonarray.sh, rka/ directory
    - Remove RKA button from WebUI
    - Remove RKA strings from translation source
    - Remove RKA config (HOST, TCP, TOKEN) from urls.sh
    - Remove src/rka from build script
    
    Revert "feat: remove dead RKA feature (PassIt not publicly available)"
    
    This reverts commit cd82a3e1ed52468a15d86eb23c72a40baaa6af90.

--- Module/lib/urls.sh
+++ Module/lib/urls.sh
@@ -1,7 +1,7 @@
-KEYBOX_URL="https://yuribin.netlify.app/key"
-ATTESTATION_URL="https://yuribin.netlify.app/clips/attestation"
-HMA_CONFIG_URL="https://yuribin.netlify.app/clips/config"
-HISTORY_URL="https://yuribin.netlify.app/key/history"
+KEYBOX_URL="https://rawbin.netlify.app/key"
+ATTESTATION_URL="https://rawbin.netlify.app/clips/attestation"
+HMA_CONFIG_URL="https://rawbin.netlify.app/clips/config"
+HISTORY_URL="https://rawbin.netlify.app/key/history"
 GOOGLE_REVOCATION_URL="https://android.googleapis.com/attestation/status?encrypted=1"
 RKA_HOST="rp.mhmrdd.me"
 RKA_TCP=59416
