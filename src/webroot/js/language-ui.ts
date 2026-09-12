becf76df83c35b23af655eaed6b623a84aeeb542
dpejoh <me@dpejoh.com>
2026-09-12 01:39:23 +0100

    ui: show english name in language options supporting text

--- src/webroot/js/language-ui.ts
+++ src/webroot/js/language-ui.ts
@@ -43,7 +43,7 @@ export async function openLanguageDialog() {
           title: item.native,
           description: item.code === 'auto'
             ? t('settings_theme_status_auto', 'Follow system')
-            : (item.native !== item.name ? item.name : undefined),
+            : item.name,
           selected: item.code === currentCode,
           onSelect: async (val: string) => {
             await applyLanguage(val);
